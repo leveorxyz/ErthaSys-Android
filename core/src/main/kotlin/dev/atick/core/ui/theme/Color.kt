7f7f6f1c35e41956c882b45209b4570e133baca0
Atick Faisal <atickfaisal@gmail.com>
2022-09-17 19:22:21 +0300

    Fix colors

--- core/src/main/kotlin/dev/atick/core/ui/theme/Color.kt
+++ core/src/main/kotlin/dev/atick/core/ui/theme/Color.kt
@@ -4,4 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val Primary = Color(0xFF_35675F)
 val PrimaryVariant = Color(0xFF_1C3238)
-val Secondary = Color(0xFF_ACD113)
+val Secondary = Color(0xFF_1C3238) // Color(0xFF_ACD113)
