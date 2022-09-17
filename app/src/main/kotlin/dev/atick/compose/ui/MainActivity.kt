e32708977e483c8348103074cf7b006151078739
Atick Faisal <atickfaisal@gmail.com>
2022-09-17 16:29:27 +0300

    Add login screen

--- app/src/main/kotlin/dev/atick/compose/ui/MainActivity.kt
+++ app/src/main/kotlin/dev/atick/compose/ui/MainActivity.kt
@@ -1,7 +1,15 @@
 package dev.atick.compose.ui
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import dev.atick.compose.R
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity()
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
